13a78c843068e6cdd38001fd3f16d3a95ad98f17
luchitha21 <66722349+luchitha21@users.noreply.github.com>
2021-12-28 13:33:44 +0530

    added service class and methods

---
+++ src/main/java/luchitha/springbootproject/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package luchitha.springbootproject.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/luchitha/springbootproject/employeemanager/repo/EmployeeRepo.java
+++ src/main/java/luchitha/springbootproject/employeemanager/repo/EmployeeRepo.java
@@ -3,5 +3,10 @@ package luchitha.springbootproject.employeemanager.repo;
 import luchitha.springbootproject.employeemanager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepo extends JpaRepository<Employee,Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
 }
